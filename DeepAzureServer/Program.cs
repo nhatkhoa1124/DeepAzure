5c3aa36dd8db0c6be6bd89da046f5d9836805ad9
Nhật Khoa Trần Nguyễn <nhatkhoa1124@gmail.com>
2026-02-07 18:20:17 +0700

    feat: Add Jwt and Authentication settings for project.

--- DeepAzureServer/Program.cs
+++ DeepAzureServer/Program.cs
@@ -1,7 +1,13 @@
 using DeepAzureServer.Data;
+using DeepAzureServer.Data.Configurations;
+using DeepAzureServer.Models.Entities;
 using DeepAzureServer.Services.Implementations;
 using DeepAzureServer.Services.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace DeepAzureServer
 {
@@ -11,32 +17,58 @@ namespace DeepAzureServer
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddControllers();
-            builder.Services.AddOpenApi();
+            builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwtSettings"));
             builder.Services.AddDbContext<AppDbContext>(options =>
                            options.UseNpgsql(
                                builder.Configuration.GetConnectionString("DefaultConnection"),
                                npgsqlOptions => npgsqlOptions.MigrationsAssembly("DeepAzureServer")
                            ));
+            builder.Services.AddIdentity<User, IdentityRole<long>>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                var jwtSettings = builder.Configuration.GetSection("JwtSettings").Get<JwtSettings>();
+                var key = Encoding.ASCII.GetBytes(jwtSettings!.SecretKey);
+
+                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidIssuer = jwtSettings.Issuer,
+                    ValidateAudience = true,
+                    ValidAudience = jwtSettings.Audience,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+
+            builder.Services.AddControllers();
+            builder.Services.AddOpenApi();
             builder.Services.AddHealthChecks()
                 .AddNpgSql(builder.Configuration.GetConnectionString("DefaultConnection")!);
 
-            // Scoped DI
             builder.Services.AddScoped<IAuthService, AuthService>();
 
             var app = builder.Build();
             app.MapHealthChecks("/health");
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
             }
 
             app.UseHttpsRedirection();
-            app.UseAuthorization();
-            app.MapControllers();
 
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllers();
             app.Run();
         }
     }
--- DeepAzureServer/Services/Implementations/AuthService.cs
+++ DeepAzureServer/Services/Implementations/AuthService.cs
@@ -1,8 +1,23 @@
 ﻿using DeepAzureServer.Services.Interfaces;
+using Microsoft.AspNetCore.Identity;
 
 namespace DeepAzureServer.Services.Implementations
 {
     public class AuthService : IAuthService
     {
+        public Task<string> GenerateJwtToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string?> Login()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IdentityResult> RegisterAsync()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
