7c4d3fdef5d54bb2ebe988e0b12df6670f0072e7
Nhật Khoa Trần Nguyễn <nhatkhoa1124@gmail.com>
2026-02-08 19:39:49 +0700

    feat: Add auth controller

--- DeepAzureServer/Controllers/AuthController.cs
+++ DeepAzureServer/Controllers/AuthController.cs
@@ -1,8 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DeepAzureServer.Models.Requests;
+using DeepAzureServer.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DeepAzureServer.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequest request)
+        {
+            var result = await _authService.RegisterAsync(request);
+            if (!result.Success) return BadRequest(result);
+
+            return Ok(result);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        {
+            var result = await _authService.LoginAsync(request);
+            if (!result.Success) return BadRequest(result);
+
+            return Ok(result);
+        }
+
     }
 }
