7c372523c5dee77034c1b73e6ed8de7ec61706c2
Nhật Khoa Trần Nguyễn <nhatkhoa1124@gmail.com>
2026-02-09 18:27:22 +0700

    feat: Add basic setup and unit tests for Login and Register in AuthService.

---
+++ DeepAzureServer.Tests/Helpers/MockHelpers.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeepAzureServer.Tests.Helpers
+{
+    public static class MockHelpers
+    {
+        public static Mock<UserManager<TUser>> MockUserManager<TUser>() where TUser : class
+        {
+            var store = new Mock<IUserStore<TUser>>();
+            return new Mock<UserManager<TUser>>(
+                store.Object, null, null, null, null, null, null, null, null);
+        }
+    }
+}
---
+++ DeepAzureServer.Tests/Services/AuthServiceTests.cs
@@ -0,0 +1,109 @@
+﻿using DeepAzureServer.Data.Configurations;
+using DeepAzureServer.Models.Entities;
+using DeepAzureServer.Models.Requests;
+using DeepAzureServer.Services.Implementations;
+using DeepAzureServer.Tests.Helpers;
+using FluentAssertions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeepAzureServer.Tests.Services
+{
+    public class AuthServiceTests
+    {
+        private readonly Mock<UserManager<User>> _userManagerMock;
+        private readonly AuthService _authService;
+
+        public AuthServiceTests()
+        {
+            _userManagerMock = MockHelpers.MockUserManager<User>();
+
+            var jwtSettings = new JwtSettings
+            {
+                SecretKey = "SuperSecretKeyForTestingOnly12345!",
+                Issuer = "DeepAzureTest",
+                Audience = "DeepAzureClient",
+                AccessTokenExpirationMinutes = 60
+            };
+
+            var optionsMock = Options.Create(jwtSettings);
+            _authService = new AuthService(_userManagerMock.Object, optionsMock);
+        }
+
+        // --- REGISTER TESTS ---
+        [Fact]
+        public async Task RegisterAsync_ShouldReturnSuccess_WhenIdentitySucceeds()
+        {
+            var request = new RegisterRequest()
+            {
+                Email = "correctregister@gmail.com",
+                Password = "StrongPassword123!"
+            };
+
+            _userManagerMock.Setup(x => x.FindByEmailAsync(request.Email))
+                .ReturnsAsync((User) null); // This checks for existing email
+            _userManagerMock.Setup(x => x.CreateAsync(It.IsAny<User>(), It.IsAny<string>()))
+                .ReturnsAsync(IdentityResult.Success);
+            _userManagerMock.Setup(x => x.AddToRoleAsync(It.IsAny<User>(), It.IsAny<string>()))
+                .ReturnsAsync(IdentityResult.Success);
+
+            var result = await _authService.RegisterAsync(request);
+
+            result.Success.Should().BeTrue();
+            result.Errors.Should().BeNullOrEmpty();
+            result.Token.Should().NotBeNullOrEmpty();
+        }
+
+        [Fact]
+        public async Task RegisterAsync_ShouldReturnErrors_WhenIdentityFails()
+        {
+            var request = new RegisterRequest()
+            {
+                Email = "incorrect-email",
+                Password = "unsafe-incorrect-password@@"
+            };
+
+            _userManagerMock.Setup(x => x.FindByEmailAsync(request.Email))
+                .ReturnsAsync((User) null);
+            var expectedError = new IdentityError { Description = "Password too weak"};
+            _userManagerMock.Setup(x => x.CreateAsync(It.IsAny<User>(), It.IsAny<string>()))
+                .ReturnsAsync(IdentityResult.Failed(expectedError));
+            _userManagerMock.Setup(x => x.AddToRoleAsync(It.IsAny<User>(), It.IsAny<string>()))
+                .ReturnsAsync(IdentityResult.Failed(expectedError));
+
+            var result = await _authService.RegisterAsync(request);
+
+            result.Success.Should().BeFalse();
+            result.Errors.Should().Contain("Password too weak");
+            result.Token.Should().BeNullOrEmpty();
+        }
+
+        // --- LOGIN TESTS ---
+        [Fact]
+        public async Task LoginAsync_ShouldReturnTrue_WhenCredentialsAreCorrect()
+        {
+            var request = new LoginRequest()
+            {
+                Email = "correctlogin@gmail.com",
+                Password = "StrongPassword123!"
+            };
+
+            _userManagerMock.Setup(x => x.FindByEmailAsync(request.Email))
+                .ReturnsAsync(new User { Email = request.Email, UserName = "Customer" });
+            _userManagerMock.Setup(x => x.CheckPasswordAsync(It.IsAny<User>(), It.IsAny<string>()))
+                .ReturnsAsync(true);
+
+            var result = await _authService.LoginAsync(request);
+
+            result.Success.Should().BeTrue();
+            result.Errors.Should().BeNullOrEmpty();
+            result.Token.Should().NotBeNullOrEmpty();
+        }
+    }
+}
