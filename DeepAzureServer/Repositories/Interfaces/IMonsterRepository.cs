5a4aa8d54a988f59297e30c33833013615200b80
Nhật Khoa Trần Nguyễn <nhatkhoa1124@gmail.com>
2026-02-16 22:16:06 +0700

    feat: Add interfaces for monster and technique.

---
+++ DeepAzureServer/Repositories/Interfaces/IMonsterRepository.cs
@@ -0,0 +1,12 @@
+﻿using DeepAzureServer.Models.Common;
+using DeepAzureServer.Models.Entities;
+
+namespace DeepAzureServer.Repositories.Interfaces
+{
+    public interface IMonsterRepository
+    {
+        public Task<PagedResult<Monster>> GetAllAsync(int pageNumber = 1, int pageSize = 20);
+        public Task<Monster?> GetByIdAsync();
+        public Task DeleteByIdAsync();
+    }
+}
---
+++ DeepAzureServer/Repositories/Interfaces/ITechniqueRepository.cs
@@ -0,0 +1,12 @@
+﻿using DeepAzureServer.Models.Common;
+using DeepAzureServer.Models.Entities;
+
+namespace DeepAzureServer.Repositories.Interfaces
+{
+    public interface ITechniqueRepository
+    {
+        public Task<PagedResult<Technique>> GetAllAsync(int pageId = 1, int pageSize = 20);
+        public Task<Technique?> GetByIdAsync(int id);
+        public Task DeleteByIdAsync(int id);
+    }
+}
