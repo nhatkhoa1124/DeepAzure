760fa3e560865c03ec59f9a8ab46b65b367604eb
Nhật Khoa Trần Nguyễn <nhatkhoa1124@gmail.com>
2026-02-18 20:27:41 +0700

    feat: Add constructor to PagedResult class.

--- DeepAzureServer/Models/Common/PagedResult.cs
+++ DeepAzureServer/Models/Common/PagedResult.cs
@@ -4,8 +4,19 @@
     {
         public IEnumerable<T> Items { get; set; } = new List<T>();
         public int TotalCount { get; set; }
-        public int PageNUmber { get; set; }
+        public int PageNumber { get; set; }
         public int PageSize { get; set; }
+
+        public PagedResult() { }
+
+        public PagedResult(IEnumerable<T> listResult, int totalCount, int pageNumber, int pageSize)
+        {
+            Items = listResult;
+            TotalCount = totalCount;
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+        }
+
         public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
     }
 }
