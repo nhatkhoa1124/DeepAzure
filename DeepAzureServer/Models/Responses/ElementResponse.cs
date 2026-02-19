7032f9f3cde9ef5a509b33382af4ec1688c6b822
Nhật Khoa Trần Nguyễn <nhatkhoa1124@gmail.com>
2026-02-19 21:25:09 +0700

    feat: Add response dtos for Ability and Element

---
+++ DeepAzureServer/Models/Responses/ElementResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace DeepAzureServer.Models.Responses
+{
+    public class ElementResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
